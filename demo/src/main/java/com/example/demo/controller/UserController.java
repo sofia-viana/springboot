6977dad93492c92f45a509ce688282516fa3b0cd
sofia-viana <sofiavianax9@gmail.com>
2026-05-09 22:46:07 +0000

    aula 08/05

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -3,9 +3,16 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.UserModel;
+import com.example.demo.service.UserService;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -13,11 +20,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    @Autowired
     UserService UserService;
     
     @GetMapping("/allUsers")
     public List<UserModel> getAllUsers(){
         return UserService.getAll();
+    }
+
+    @PostMapping("/add")
+    public UserModel newUser(@RequestBody UserModel user) {
+        return userService.addNewUser(user);
+    }
+    @GetMapping("/user/{id}")
+    public UserModel getUser(@PathVariable Long id){
+        return userService.getById(id);
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
     }
 }
     
--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,8 +1,26 @@
 package com.example.demo.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.model.UserModel;
+import com.example.demo.repository.UserRepository;
 
 @Service
 public class UserService {
+    
+    @Autowired
+    UserRepository userRepository;
+
+    public List<UserModel> getAllUsers(){
+        return userRepository.findAll();
+    }
+    public UserModel addNewUser(UserModel user) {
+        return userRepository.save(user);
+    }
+    public UserModel getById(Long id){
+        return userRepository.findById(id).get();
+    }
     
 }
